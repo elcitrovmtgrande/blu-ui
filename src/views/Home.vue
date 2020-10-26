3f1c070ea32cbfd1d892914b8e1b678b132a8e82
elcitrovmtgrande <victor.mahe.pro@gmail.com>
2020-10-26 22:39:33 +0100

    Adding SCSS preprocessor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="main">
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  height: 100%;
+  background: yellow;
+}
+</style>
